ca6c081ea1adc5e54f52e0d4cd15d4e69a907dae
kevinsims1 <kevinsims1@outlook.com>
2019-10-29 15:14:07 -0500

    cookbook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 import RecipeCard from './RecipeCard';
+import "./App.css"
 
 function App() {
+  const APP_ID = "41a123e6"
+  const APP_KEY = "4102a6960f90afe8e1f3d4f2ad201ee7"
+
+  const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('chicken')
+
+  useEffect(() => {
+    getRecipes()
+}, [query])
+
+  const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+      const data = await response.json();
+      setRecipes(data.hits);
+      console.log(data.hits)
+  }
+
+  const onChange = (e) =>{
+      setSearch(e.target.value)
+  } 
+
+  const getSearch = e => {
+    e.preventDefault()
+    setQuery(search)
+  }
+
   return (
-    <RecipeCard/>
+    <div className="App">
+      <form className="searchForm" onSubmit={getSearch}>
+          <input className="searchBar" type='text' value={search} onChange={onChange}/>
+          <button className="searchButton" type="submit"> Search </button>
+      </form>
+
+      <div className="recipes">
+      {recipes.map(recipe => (<RecipeCard title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients}/>))}
+      </div>
+    </div>
   );
 }
 
--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,9 +1,15 @@
 import React from "react"
+import style from './RecipeCard.module.css'
 
-function RecipeCard(){
+function RecipeCard({title, calories, image, ingredients}){
     return(
-        <div>
-            "HI"
+        <div className={style.recipes}>
+            <img src={image} alt=""/>
+            <h1>{title}</h1>
+            <p>{calories}</p>
+            <ol>{ingredients.map((ingredient)=>(
+                <li>{ingredient.text}</li>
+            ))}</ol>
         </div>
     )
 }
